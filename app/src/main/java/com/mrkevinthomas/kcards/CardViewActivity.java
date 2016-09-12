c3928d7638a9a044f3e968db8428247bba01a223
Kevin Thomas <unparalleled@users.noreply.github.com>
2016-09-11 17:43:11 -0700

    added basic text-to-speach functionality for Locale.US

--- app/src/main/java/com/mrkevinthomas/kcards/CardViewActivity.java
+++ app/src/main/java/com/mrkevinthomas/kcards/CardViewActivity.java
@@ -1,10 +1,13 @@
 package com.mrkevinthomas.kcards;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import com.mrkevinthomas.kcards.models.Deck;
+
+import java.util.Locale;
 
 public class CardViewActivity extends BaseActivity {
 
@@ -13,6 +16,8 @@ public class CardViewActivity extends BaseActivity {
 
     private ViewPager viewPager;
     private CardViewAdapter cardViewAdapter;
+
+    private TextToSpeech textToSpeech;
 
     @Override
     protected boolean shouldShowUpButton() {
@@ -30,9 +35,26 @@ public class CardViewActivity extends BaseActivity {
         recyclerView.setVisibility(View.GONE);
         fab.setVisibility(View.GONE);
 
-        cardViewAdapter = new CardViewAdapter(deck);
+        cardViewAdapter = new CardViewAdapter(deck, this);
         viewPager.setAdapter(cardViewAdapter);
         viewPager.setCurrentItem(position);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+        });
+        textToSpeech.setLanguage(Locale.US);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        textToSpeech.shutdown();
+    }
+
+    public TextToSpeech getTextToSpeech() {
+        return textToSpeech;
     }
 
 }
--- app/src/main/java/com/mrkevinthomas/kcards/CardViewAdapter.java
+++ app/src/main/java/com/mrkevinthomas/kcards/CardViewAdapter.java
@@ -1,5 +1,6 @@
 package com.mrkevinthomas.kcards;
 
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
@@ -12,22 +13,20 @@ import android.widget.TextView;
 import com.mrkevinthomas.kcards.models.Card;
 import com.mrkevinthomas.kcards.models.Deck;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CardViewAdapter extends PagerAdapter {
 
-    private List<Card> cardList = new ArrayList<>();
     private Deck deck;
 
-    public CardViewAdapter(@NonNull Deck deck) {
-        this.cardList = deck.getCards();
+    private CardViewActivity cardViewActivity;
+
+    public CardViewAdapter(@NonNull Deck deck, @NonNull CardViewActivity cardViewActivity) {
         this.deck = deck;
+        this.cardViewActivity = cardViewActivity;
     }
 
     @Override
     public int getCount() {
-        return cardList.size();
+        return deck.size();
     }
 
     @Override
@@ -38,14 +37,28 @@ public class CardViewAdapter extends PagerAdapter {
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         View itemView = LayoutInflater.from(container.getContext()).inflate(R.layout.card_view_item, container, false);
-        Card card = cardList.get(position);
+        final Card card = deck.getCards().get(position);
 
         TextView frontText = (TextView) itemView.findViewById(R.id.card_front_text);
         TextView backText = (TextView) itemView.findViewById(R.id.card_back_text);
         WebView webView = (WebView) itemView.findViewById(R.id.web_view);
 
         frontText.setText(card.getFrontText());
+        itemView.findViewById(R.id.card_front_holder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardViewActivity.getTextToSpeech().speak(card.getFrontText(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
         backText.setText(card.getBackText());
+        itemView.findViewById(R.id.card_back_holder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardViewActivity.getTextToSpeech().speak(card.getBackText(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
         setupWebView(webView, card);
 
         container.addView(itemView, 0);
@@ -64,6 +77,7 @@ public class CardViewAdapter extends PagerAdapter {
             public void onPageFinished(WebView view, String url) {
                 // hide page headers
                 view.loadUrl("javascript: document.getElementById('main').scrollIntoView();");
+                view.loadUrl("javascript: document.getElementById('ires').scrollIntoView();");
                 //view.loadUrl("javascript: document.getElementById('sfcnt').style.display = 'none';");
                 //view.loadUrl("javascript: document.getElementById('taw').style.display = 'none';");
             }
